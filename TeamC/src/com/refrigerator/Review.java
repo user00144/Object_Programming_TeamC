bca58a21e34dc49965c31802a6a83f36e4555547
hwanj <hwanj@DESKTOP-DGNO7M7>
2023-11-09 19:26:30 +0900

    Review관련 필드 메소드 이동

--- TeamC/src/com/refrigerator/Review.java
+++ TeamC/src/com/refrigerator/Review.java
@@ -1,19 +1,43 @@
 package com.refrigerator;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import mgr.Manageable;
 
 public class Review implements Manageable{
+	
+	ArrayList<String> reviewList = new ArrayList<>();
+	double score;
 
 	@Override
 	public void print() {
-		// TODO Auto-generated method stub
-		
+		// 평점 출력
+				/*System.out.format("%s | %s | %.1f/5.0 \n", name, cookName, score);
+				for (String s : reviewList) {
+					System.out.print(s + "\n");
+				}*/
 	}
 
 	@Override
 	public void read(Scanner scan) {
-		// TODO Auto-generated method stub
+		// 평점 입력 및 관리
+
+		double n;
+		String s;
+		while (true) {
+			System.out.print("이 레시피의 평점을 입력해 주세요. (0.0 ~ 5.0)");
+			n = scan.nextDouble();
+			if (n < 0.0 || n > 5.0) {
+				System.out.print("입력할 수 없는 평점입니다!");
+				continue;
+			}
+			score = 0.725 * score + 0.275 * n;
+			break;
+		}
+
+		System.out.print("이 레시피의 한 줄 평을 입력해 주세요.");
+		s = scan.next();
+		reviewList.add(s);
 		
 	}
 
@@ -21,6 +45,10 @@ public class Review implements Manageable{
 	public boolean matches(String kwd) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	void printReview() {
+		
 	}
 
 }
