28d79589253e0a7ea6b39a29d6a5570542b6fe60
hwanj <hwanj@DESKTOP-DGNO7M7>
2023-11-18 17:10:28 +0900

    리뷰클래스 수정 및 완성

--- TeamC/src/com/refrigerator/Review.java
+++ TeamC/src/com/refrigerator/Review.java
@@ -17,58 +17,36 @@ public class Review implements Manageable,UIData{
 		this.recipe = recipe;
 		recipe.rvList.add(this);
 	}
+	
 	@Override
 	public void print() {
 		// 평점 출력
-				System.out.format("%s | %s | %d \n", recipe.name, recipe.cookName, score);
-					System.out.print(review + "\n");
-
+				System.out.format("%d\n", score);
+				System.out.print(review + "\n");
 	}
 
 	@Override
 	public void read(Scanner scan) {
 		// 평점 입력 및 관리
-		
-		String s;
-		while (true) {
-			System.out.print("이 레시피의 평점을 입력해 주세요. (0 ~ 5)");
-			score = scan.nextInt();
-			if (score < 0 || score > 5) {
-				System.out.print("입력할 수 없는 평점입니다!");
-				continue;
-			}
-			 
-			break;
-		}
-
-		System.out.print("이 레시피의 한 줄 평을 입력해 주세요.");
-		s = scan.next();
-		review = s;
+		review = scan.nextLine();
+		score = scan.nextInt();
 	}
 	
 	public boolean matches(String kwd) {
-		// TODO Auto-generated method stub
-		
 		return false;
-	}
-	
-	void printReview(ArrayList <String> mList) {
-		System.out.println(" === 리뷰 === ");
-		for (String review : mList) {
-			System.out.println(review);
-		}
 	}
 
 	@Override
 	public void set(Object[] uitexts) {
 		// TODO Auto-generated method stub
-		
+		review = (String)uitexts[0];
+		score = Integer.parseInt((String)uitexts[1]);
 	}
 
 	@Override
 	public String[] getUiTexts() {
 		// TODO Auto-generated method stub
-		return null;
+		return new String[] {review, ""+score};
 	}
 
 }
